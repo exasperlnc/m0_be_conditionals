0e9f0bf9914eb7163dcae3416569c9f40b09ca34
Logan Cole <emtlogancole@gmail.com>
2022-12-05 23:02:25 -0600

    Complete Medium Challenge

---
+++ medium_challenge.rb
@@ -0,0 +1,11 @@
+good_driving_record = true
+age = 24
+
+if good_driving_record = true and age > 25
+    puts 'You get a discount!'
+elsif good_driving_record = true or age > 25
+    puts 'You pay full price, not too shabby.'
+else
+    puts 'Someone else will have to sign for the rental, sorry!' 
+end 
+
